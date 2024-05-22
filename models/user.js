073b37823767db693084ece2ddebd779d821ade0
Schlenge <128624796+Schlenge@users.noreply.github.com>
2024-05-22 10:52:39 +0300

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,27 @@
+// const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
